416b2ff75b0aba4fe8523b66723d40a07545870a
swaran <swaran@autozang.com>
2021-02-22 22:22:12 +0530

    firstt commit

---
+++ ppmtool/src/test/java/com/io/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
